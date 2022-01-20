f576b1cb0122a7fd33344a8142de6551673549e2
Pablo Calderon salazar <pablocalderonsalazar@MacBook-Air-de-Pablo.local>
2022-01-20 16:08:26 -0500

    Question and Choice models were created

--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
