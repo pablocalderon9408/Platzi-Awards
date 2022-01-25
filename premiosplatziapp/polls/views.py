5c81bc1e17be62175a7940508fe35c54fa1949dc
Pablo Calderon <pablocalderon9408@gmail.com>
2022-01-24 22:49:38 -0500

    vote view added

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -18,7 +18,10 @@ def detail(request, question_id):
     })
 
 def results(request, question_id):
-    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, template_name='polls/results.html', context={
+        "question": question
+    })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
