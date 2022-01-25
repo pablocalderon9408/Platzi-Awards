726c3503605984c7e9d7180c489d9bac2cca46a1
Pablo Calderon <pablocalderon9408@gmail.com>
2022-01-24 22:36:48 -0500

    25/ vote view enabled

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from jinja2 import TemplateError
-from polls.models import Question
+from polls.models import Choice, Question
 # Create your views here.
 
 def index(request):
@@ -20,5 +21,17 @@ def results(request, question_id):
     return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, template_name='polls/detail.html', context={
+            'question': question,
+            'error_message': "No elegiste una respuesta"
+        })
+    else:
+        import ipdb ; ipdb.set_trace()
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
