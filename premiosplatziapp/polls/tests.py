e0465b222e776705b2d8bdfa0132042eb801fe06
Pablo Calderon <pablocalderon9408@gmail.com>
2022-04-19 18:52:11 -0500

    start with tests in the app

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Question
+
+class QuestionModelTests(TestCase):
+    """"""
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="Quién es el mejor CD de platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_now_question(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now()
+        future_question = Question(question_text="Quién es el mejor CD de platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_past_question(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() - datetime.timedelta(days=2)
+        future_question = Question(question_text="Quién es el mejor CD de platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
